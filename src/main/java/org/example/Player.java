f190f0460e634a468dfe4e0a67de971aef64550c
cjgarcia12 <christian.jgarcia03@gmail.com>
2024-08-01 14:55:24 -0400

    small changes

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -5,12 +5,10 @@ import java.util.ArrayList;
 
 //implementation of blackjack player
 public class Player {
-    private int score;
     private ArrayList<Card> cards;
     private int money;
 
     public Player() {
-        this.score = 0;
         this.cards = new ArrayList<>();
         this.money = 1000;
     }
@@ -21,17 +19,6 @@ public class Player {
 
     public void setMoney(int money) {
         this.money = money;
-    }
-
-
-    public int getScore(int score) {
-        return this.score;
-    }
-
-
-    @Override
-    public String toString() {
-        return "cards: " + this.cards + "\nScore: " + this.score;
     }
 
 
